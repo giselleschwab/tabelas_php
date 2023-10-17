eb49f98100f2b1a9d2eee62ccff1750a4bbb9e73
Giselle Schwab Silva <giselleschwab@gmail.com>
2023-10-17 20:58:36 -0300

    feat: add filter data range

--- views/cursos.php
+++ views/cursos.php
@@ -1,9 +1,20 @@
 <a href="?pagina=inserir_cursos" class="btn btn-success">Inserir novo curso</a>
+<table cellspacing="5" cellpadding="5">
+        <tbody><tr>
+            <td>Minimum date:</td>
+            <td><input type="text" id="min" name="min"></td>
+        </tr>
+        <tr>
+            <td>Maximum date:</td>
+            <td><input type="text" id="max" name="max"></td>
+        </tr>
+    </tbody></table>
 <table class="table table-hover table-striped" id="cursos">
   <thead>
     <tr>
       <th scope="col">Nome</th>
       <th scope="col">Duração</th>
+      <th scope="col">Data nascimento</th>
       <th>Editar</th>
       <th>Deletar</th>
     </tr>
@@ -13,6 +24,7 @@
         while($linha = mysqli_fetch_array($consulta_cursos)){
             echo'<tr><td>'.$linha['NOME'].'</td>';
             echo'<td>'.$linha['DURACAO'].'</td>';
+            echo'<td>'.$linha['data_nascimento'].'</td>';
       ?>
       <td><a href="?pagina=inserir_cursos&editar=<?php echo $linha['ID']; ?>">Editar</a></td>
       <td><a href="deleta_curso.php?id_curso=<?php echo $linha['ID']; ?>">Deletar</a></td>
