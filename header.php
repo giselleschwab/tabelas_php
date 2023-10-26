e8124fb3cc50c1f2c91d0f17d22481dade286427
Giselle Schwab Silva <giselleschwab@gmail.com>
2023-10-25 21:31:56 -0300

    feat: change css header

--- header.php
+++ header.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title>Dog Cursos</title>
+    <link rel="stylesheet" href="css/style.css"/>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.css" />
     <link rel="stylesheet" href=//cdn.datatables.net/1.13.6/css/jquery.dataTables.min.css />
@@ -11,7 +12,7 @@
    
 
   </head>
-  <body class="bg-dark text-light">
+  <!-- <body class="bg-dark text-light">
     <div class="container">
         <div class="jumbotron bg-danger">
         <nav class="navbar navbar-expand-lg bg-danger">
@@ -42,4 +43,30 @@
                 </ul>
             </div>
             </nav>
-        </div>   
+        </div>    -->
+
+
+<nav class="navbar navbar-expand-lg bg-body-tertiary" data-bs-theme="dark">
+  <div class="container-fluid">
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="?pagina=home">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="?pagina=cursos">Cursos</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="?pagina=alunos">Alunos</a>
+        </li>
+      </ul>
+      <form class="d-flex" role="search">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
